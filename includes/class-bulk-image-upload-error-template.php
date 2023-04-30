4d5c3b7c0b44878d98227f76bafa7d9d216fd0cf
Farid Movsumov <faridmovsumov@gmail.com>
2023-04-30 17:48:48 +0200

    security

--- includes/class-bulk-image-upload-error-template.php
+++ includes/class-bulk-image-upload-error-template.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
 class ErrorTemplate
 {
     public static function showErrorTemplate($errorMessage)
--- includes/class-bulk-image-upload-status-color.php
+++ includes/class-bulk-image-upload-status-color.php
@@ -1,5 +1,9 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
 class StatusColor
 {
     const JOB_STATUS_PENDING = 'pending';
