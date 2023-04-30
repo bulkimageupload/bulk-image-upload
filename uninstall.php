a1cbf6f6e220498c4285bec245dcc87e11091318
Farid Movsumov <faridmovsumov@gmail.com>
2023-04-30 20:51:28 +0200

    coding standards

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * Removing option which was added to options during activation.
+ *
+ * @package bulk-image-upload-for-woocommerce
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
