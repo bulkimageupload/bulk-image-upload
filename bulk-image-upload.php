f670f905a9cf73f7a43dcc143a3cecc81530f2a1
Farid Movsumov <faridmovsumov@gmail.com>
2023-07-01 08:05:59 +0200

    debug

--- bulk-image-upload.php
+++ bulk-image-upload.php
@@ -261,6 +261,9 @@ function bulk_image_upload_render_create_new_upload_page() {
 	$body = wp_remote_retrieve_body( $response );
 	$data = json_decode( $body, true );
 
+	echo "<pre>";
+	var_dump($data);exit;
+
 	load_template(
 		plugin_dir_path( __FILE__ ) . 'admin/partials/bulk-image-upload-create-new-upload.php',
 		true,
