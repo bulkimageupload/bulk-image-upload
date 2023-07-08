db894b610e31e27af0399b2463ee2c783bdd7e7d
Farid Movsumov <faridmovsumov@gmail.com>
2023-07-08 18:14:48 +0200

    chatwoot added

--- admin/partials/bulk-image-upload-dashboard.php
+++ admin/partials/bulk-image-upload-dashboard.php
@@ -158,5 +158,22 @@ $bulk_image_upload_key    = $args['key'];
 				jQuery('head').append('<meta http-equiv="refresh" content="5"/>');
 			});
 		</script>
+
+		<script>
+            (function(d,t) {
+                var BASE_URL="https://app.chatwoot.com";
+                var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                g.src=BASE_URL+"/packs/js/sdk.js";
+                g.defer = true;
+                g.async = true;
+                s.parentNode.insertBefore(g,s);
+                g.onload=function(){
+                    window.chatwootSDK.run({
+                        websiteToken: 'zs5kEmpf18XhK4gkwEWPXLvv',
+                        baseUrl: BASE_URL
+                    })
+                }
+            })(document,"script");
+		</script>
 	</div>
 </div>
