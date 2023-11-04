40a2ef747b20609e9d6604243ee5880f22470318
Farid Movsumov <faridmovsumov@gmail.com>
2023-11-04 06:47:37 +0100

    fix bug

--- admin/partials/bulk-image-upload-matching-results.php
+++ admin/partials/bulk-image-upload-matching-results.php
@@ -23,7 +23,7 @@
             </div>
         <?php } ?>
 
-		<?php if (isset(['matching_results']['matchedImages']) && count($args['matching_results']['matchedImages']) === 0 && count($args['matching_results']['matchedImages']) === 0) { ?>
+		<?php if (isset($args['matching_results']['matchedImages']) && count($args['matching_results']['matchedImages']) === 0 && count($args['matching_results']['matchedImages']) === 0) { ?>
 			<div class="notice notice-warning biu-notice">
 				<?php esc_html_e('Images not found inside the folder', 'bulk-image-upload'); ?>
 			</div>
