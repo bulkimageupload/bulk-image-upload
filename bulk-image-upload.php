1d81422d0ba0084b91582ed6c8d380062f41baa2
Farid Movsumov <faridmovsumov@gmail.com>
2023-11-04 06:27:40 +0100

    debug

--- bulk-image-upload.php
+++ bulk-image-upload.php
@@ -239,6 +239,8 @@ function bulk_image_upload_render_matching_results() {
         wp_redirect($current_page_url);
     }
 
+    echo '<pre>';var_dump($matching_results);
+
     load_template(
 		plugin_dir_path( __FILE__ ) . 'admin/partials/bulk-image-upload-matching-results.php',
 		true,
